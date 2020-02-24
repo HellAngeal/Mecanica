23ccc05b250d43d7fc840ac983f636ffede6fe78
HellAngeal <persia.2000@hotmail.com>
2020-02-24 16:26:34 -0600

    Cosas de Mecanica

---
+++ Assets/Clases.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clases : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    [SerializeField]
+    float F1;
+
+    [SerializeField]
+    float F2;
+
+    public float D1;
+    public float D2;
+    void Start()
+    {
+       
+
+    }
+
+    float Formula(float F1,  float F2, float D1)
+    {
+
+        D2 = (F1 * D1) / F2;
+
+        return D2;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Formula(F1, F2, D1);
+
+        
+
+        Debug.Log(D2);
+        
+    }
+}
---
+++ Assets/Kelvin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Kelvin : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 0f;
+    [SerializeField] 
+    Text CountdownText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += 10 * Time.deltaTime;
+        CountdownText.text = currentTime.ToString("0");
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+    }
+}
