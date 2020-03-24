ebabb8397e91085e600bdf2d3d56ae622e4ad06f
HellAngeal <persia.2000@hotmail.com>
2020-03-23 19:23:24 -0600

    COSAS

---
+++ Assets/Scripts/Examen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Examen : MonoBehaviour
+{
+    public InputField Sonic;
+    public InputField Knuckles;
+
+    float VSonic;
+    float VKnuckles;
+    float VFinal;
+    float Tiempo;
+    float DSonic;
+    float DKnuckles;
+
+    public void Calcular()
+    {
+        VSonic = float.Parse(Sonic.text);
+        VKnuckles = float.Parse(Knuckles.text);
+        VFinal = VSonic + VKnuckles;
+        Tiempo = 500 / VFinal;
+        DSonic = Tiempo * VSonic;
+        DKnuckles = Tiempo * VKnuckles;
+        Debug.Log("Chocan en la distancia: "+DSonic+"de Sonic y la distancia: "+DKnuckles+"de Knuckles, teniendo en cuenta que ambos avanzan al centro.");
+    }
+}
