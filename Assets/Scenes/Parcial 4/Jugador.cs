c58cfddc3dfcb065b01196b54afaba4df342bd32
Ivanmortr <57369225+Ivanmortr@users.noreply.github.com>
2020-06-12 02:46:18 -0500

    ProyectoFinalMecanicaIvancin

--- Assets/Scenes/Parcial 4/Jugador.cs
+++ Assets/Scenes/Parcial 4/Jugador.cs
@@ -4,21 +4,60 @@ using UnityEngine;
 
 public class Jugador : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    #region variables Aceleracion = F / M;
+    [SerializeField] float fuerza = 5f;
+    [SerializeField] float masa = 1f;
+    #endregion
+
+    const float gravedad = 9.81f;
+
+
+    #region Friccion = u * N;
+    public float coeficienteFriccion;
+    public float Normal;
+    public float fuerzaFriccion;
+    #endregion
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * moveSpeed;
+        Aceleracion(fuerza, masa,fuerzaFriccion);
+        Debug.Log("Coeficiente Friccion: " + coeficienteFriccion + "FuerzaFriccion: " + fuerzaFriccion);
     }
-    void Jump()
+
+
+    //Deteccion de materiales para sacar los coeficiente
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-       
+        
+        if (collision.gameObject.CompareTag("Hielo"))
+        {
+            coeficienteFriccion = 0.09f;
+        }
+        else if(collision.gameObject.CompareTag("Pasto"))
+        {
+            coeficienteFriccion = 0.35f;
+        }
+        FuerzaFriccion(coeficienteFriccion, masa);
+        
+    }
+
+    //FORMULA PARA SACAR FUERZA DE FRICCION
+    void FuerzaFriccion(float coeficienteFriccion, float masa)
+    {
+         Normal = masa * gravedad;
+         fuerzaFriccion = coeficienteFriccion * Normal; 
+        
+    }
+
+    //ACELERACION Y RESTARLE LA FUERZA DE FRICCION A LA FUERZA
+    void Aceleracion(float fuerza, float masa, float fuerzaFriccion)
+    {
+        fuerza -= fuerzaFriccion;
+        float aceleracionF = fuerza / masa;
+
+        Debug.Log("aceleracion: "+aceleracionF);
+        transform.position += Vector3.right * aceleracionF * Time.deltaTime;
     }
 }
