33eab676de5ab8fe808d525f7fa98076c5fc3cfb
Domtic <45299959+Domtic@users.noreply.github.com>
2020-06-24 00:24:09 -0500

    Menus

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour
+{
+   public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void lvl1()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void lvl2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void lvl3()
+    {
+        SceneManager.LoadScene(5);
+    }
+}
---
+++ Assets/Scenes/Parcial 4/Scripts/SelectionLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SelectionLevel : MonoBehaviour
+{
+    public void GoToLoadLevl()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+}
